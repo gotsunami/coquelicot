987fee56196f77190e2003e1a518e60d0c7c1e0b
matm <mathias@monnerville.com>
2015-08-26 22:50:42 +0200

    Move file copying code to base file manager

--- attachment/file_manager.go
+++ attachment/file_manager.go
@@ -1,6 +1,9 @@
 package attachment
 
 import (
+	"fmt"
+	"io"
+	"os"
 	"path/filepath"
 	"strconv"
 	"time"
@@ -42,6 +45,63 @@ func (fbm *FileBaseManager) Filepath() string {
 
 func (fbm *FileBaseManager) Url() string {
 	return filepath.Join(fbm.Dir.Path, fbm.Filename)
+}
+
+// copyFile copies a file from src to dst. If src and dst files exist, and are
+// the same, then return success. Otherwise copy the file contents from src to dst.
+func (fbm *FileBaseManager) copyFile(src, dst string) error {
+	sfi, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !sfi.Mode().IsRegular() {
+		// cannot copy non-regular files (e.g., directories,
+		// symlinks, devices, etc.)
+		// FIXME
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+	}
+	dfi, err := os.Stat(dst)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	} else {
+		if !(dfi.Mode().IsRegular()) {
+			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+		}
+		if os.SameFile(sfi, dfi) {
+			return err
+		}
+	}
+	return fbm.copyFileContents(src, dst)
+}
+
+// copyFileContents copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file.
+func (fbm *FileBaseManager) copyFileContents(src, dst string) error {
+	var err error
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+	err = out.Sync()
+	return err
 }
 
 func seconds() int64 {
